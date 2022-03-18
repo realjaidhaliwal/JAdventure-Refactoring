8ad9863b70612a0a80a482bd520c05442be0064c
Karen McDougall <karenmcdougall@uvic.ca>
2022-03-17 21:05:04 -0700

    Change name of new method from getEquipmentLocation to checkNewEquipmentLocation

--- src/main/java/com/jadventure/game/entities/Entity.java
+++ src/main/java/com/jadventure/game/entities/Entity.java
@@ -190,7 +190,7 @@ public abstract class Entity {
     public Map<String, String> equipItem(EquipmentLocation place, Item item) {
         double oldDamage = this.damage;
         int oldArmour = this.armour;
-        place = getEquipmentLocation(place, item);
+        place = checkNewEquipmentLocation(place, item);
         equipment.put(place, item);
         removeItemFromStorage(item);
         Map<String, String> result = new HashMap<String, String>();
@@ -239,7 +239,7 @@ public abstract class Entity {
         return result;
     }
 
-    private EquipmentLocation getEquipmentLocation(EquipmentLocation place, Item item) {
+    private EquipmentLocation checkNewEquipmentLocation(EquipmentLocation place, Item item) {
         if (place == null) {
             place = item.getPosition();
         }
